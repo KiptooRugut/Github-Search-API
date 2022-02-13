607fe4f3f6dd20833fd2a77671f3d44e70af1749
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-13 23:42:29 +0300

    it will add github inital user information variables, member functions and methods

--- src/app/repository/repository.ts
+++ src/app/repository/repository.ts
@@ -1,2 +1,11 @@
 export class Repository {
+    constructor(
+        public name: string,
+        public description: string,
+        public updated_at: Date,
+        public clone_link: string,
+        public language: string,
+        public html_url: string,
+        public created_at: Date
+    ) { }
 }
