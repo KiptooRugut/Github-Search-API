5b318fbfd4f74e2bb78c1f86475da901aba72d1f
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-13 23:49:35 +0300

    it will define repositories titles, text color and text behaviour

--- src/app/color.directive.ts
+++ src/app/color.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appColor]'
 })
 export class ColorDirective {
 
-  constructor() { }
+  constructor(private element:ElementRef) { 
+    this.element.nativeElement.style.fontFamily = "cursive"
+    this.element.nativeElement.style.textTransform= "uppercase"
+    this.element.nativeElement.style.weight = "700"
+    this.element.nativeElement.style.color = "yellow";
+       
+  }
 
 }
