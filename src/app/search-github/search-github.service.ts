b56c200c0aaaf4ac8a119d16d90c0cd64488bf17
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-13 23:56:46 +0300

    it will add imports,  variables definitions, inject http client and define methods

--- src/app/search-github/search-github.service.ts
+++ src/app/search-github/search-github.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../users/user';
+import { Repository } from '../repository/repository';
+// import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchGithubService {
 
-  constructor() { }
+  user: User;
+  repository: Repository;
+  repositoryData:any = [];
+  newUserData: any = [];
+
+  constructor(private http: HttpClient) { 
+    this.user = new User(0, "", "", new Date, true, "");
+    this.repository = new Repository("", "", new Date, "", "", "", new Date)
+  }
+
+  getData(username: string) {
+
+    this.repositoryData.length = 0;
+
+    interface ApiResponse {
+      public_repos: number,
+      login: string,
+      avatar_url: string,
+      created_at: Date,
+      html_url: string,
+      hireable: boolean
+    }
+  } 
 }
